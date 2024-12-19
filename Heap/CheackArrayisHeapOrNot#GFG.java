eb4610bb888a44b0529c459cc52eb52cf2f32b38
sameerraj09 <138888985+sameerraj09@users.noreply.github.com>
2024-12-19 12:42:32 +0530

    Create CheackArrayisHeapOrNot#GFG.java

---
+++ Heap/CheackArrayisHeapOrNot#GFG.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean countSub(long arr[], long n)
+    {
+        for(int i=0;i<n;i++){
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+            if(left<n) if(arr[left] > arr[i]) return false;
+            if(right<n) if(arr[right] > arr[i]) return false;
+        }
+        return true;
+    }
+}
